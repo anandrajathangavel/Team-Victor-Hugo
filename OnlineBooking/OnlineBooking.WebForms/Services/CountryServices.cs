8f56775f59f66ceaca73fd2149e34dbe64c35f12
hAWKdv <hawk94@abv.bg>
2014-10-23 15:46:11 +0300

    Added countries list; Small layout changes
    - Refactored Services

---
+++ OnlineBooking/OnlineBooking.WebForms/Services/CountryServices.cs
@@ -0,0 +1,49 @@
+﻿namespace OnlineBooking.WebForms.Services
+{
+    using System.Data;
+    using System.Linq;
+    using OnlineBooking.WebForms.App_Data;
+    using OnlineBooking.WebForms.Models;
+
+    public class CountryServices : Service
+    {
+        public CountryServices()
+        {
+        }
+
+        public DataTable GetAll()
+        {
+            var countries = this.data.Counties.All();
+
+            return this.IQueryableToDataTable(countries);
+        }
+
+        public void UpdateCountry(int id, string name)
+        {
+            var selectedCountry = this.data.Counties.All().FirstOrDefault(c => c.Id == id);
+            selectedCountry.Name = name;
+
+            this.data.SaveChanges();
+        }
+
+        public override DataTable IQueryableToDataTable(IQueryable countries)
+        {
+            var dataTable = new DataTable();
+
+            dataTable.Columns.Add("ID");
+            dataTable.Columns.Add("Name");
+
+            foreach (Country country in countries)
+            {
+                var row = dataTable.NewRow();
+
+                row["ID"] = country.Id;
+                row["Name"] = country.Name;
+
+                dataTable.Rows.Add(row);
+            }
+
+            return dataTable;
+        }
+    }
+}
